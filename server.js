a1927eef1c29baa929b9266c36b8476b415710ca
Yash Narvekar <yash.narvekar01@gmail.com>
2023-02-01 19:59:01 +0530

    build commit final

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ const userRoutes = require("./routes/userRoutes");
 const path = require("path");
 app.use("/api/user/", userRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+//   });
+// }
+
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
